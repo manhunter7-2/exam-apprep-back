1435778dd3f60c4655342fa46e0e7e385f30b18d
manhunter7-2 <thomas.piquet7@gmail.com>
2023-06-19 16:47:59 +0200

    test swagger jsdoc

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+      info: {
+        title: 'Dialog API',
+        description: 'Dialog API Information',
+        contact: {
+          name: 'Developer Name',
+        },
+        servers: ['http://localhost:3000'],
+      },
+    },
+    apis: ['./routes/DialogRoutes.js'],
+  };
+  
+  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/', require('./routes/routes'))
 
 app.listen(port, console.log("Listening on port "+port))
